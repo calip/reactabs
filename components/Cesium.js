fc2220574335ad46278cbef04c8f4b298fa80f32
alip putra <asalip.putra@gmail.com>
2022-06-29 07:47:37 +0700

    add models graphics to cesium entity

--- components/Cesium.js
+++ components/Cesium.js
@@ -1,8 +1,28 @@
 
-import { Viewer } from 'resium'
+import { Viewer, Entity, ModelGraphics } from 'resium'
+import { Cartesian3, Color } from 'cesium'
+
+const position = Cartesian3.fromDegrees(106.774124, -6.200000, 100);
 
 export default function Cesium() {
   return (
-    <Viewer full />
+    <Viewer full>
+      <Entity
+                  name="Airplane"
+                  label={"Airplane"}
+                  tracked 
+                  point={{ pixelSize: 50, color: Color.BLUE }}
+                  position={position}
+        >
+          <ModelGraphics
+                      scale={1}
+                      uri={"/models/a318.glb"}
+                      minimumPixelSize={100}
+                      runAnimations
+                      show
+                      color={Color.WHITE}
+          />
+        </Entity>
+      </Viewer>
   )
 }
