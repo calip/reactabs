703b394db3070a847cb85790f6144d72a63274e9
alip putra <asalip.putra@gmail.com>
2022-07-19 14:47:52 +0700

    update resctricmode

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack: config => {
     config.plugins.push(
       new webpack.DefinePlugin({
