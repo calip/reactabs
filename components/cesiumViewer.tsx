7ca750cbfe93d1ff71e9bb1d12322398f2840a54
alip putra <asalip.putra@gmail.com>
2022-07-19 15:05:15 +0700

    add multiple entity drag and drop

--- components/cesiumViewer.tsx
+++ components/cesiumViewer.tsx
@@ -4,22 +4,23 @@ import * as Cesium from 'cesium'
 import CesiumEntity from "./cesiumEntity"
 
 
-const CesiumViewer = () => {
+const CesiumViewer = (prop: any) => {
+  const { entities } = prop
   const ref = useRef<Resium.CesiumComponentRef<Cesium.Viewer>>(null)
-
+  
   useEffect(() => {
-    if(ref.current?.cesiumElement) {
-      const position = Cesium.Cartesian3.fromDegrees(106.774124, -6.200000, 100)
-      //Actually create the entity
-      const entity = ref.current?.cesiumElement.entities.add({
-        position: position,
-        //Load the Cesium plane model to represent the entity
-        model: {
-          uri:"/models/Cesium_Air.glb",
-          minimumPixelSize: 64,
-        },
+    if(ref.current?.cesiumElement && entities) {
+      entities.map((entity: any) => {
+        const position = Cesium.Cartesian3.fromDegrees(entity.longitude, entity.latitude, entity.height)
+        ref.current?.cesiumElement?.entities.add({
+          position: position,
+          model: {
+            uri: entity.url,
+            minimumPixelSize: 64,
+          }
+        })
       })
-
+      
       const drag = new CesiumEntity(ref.current?.cesiumElement)
       drag.enable()
       
@@ -34,44 +35,3 @@ const CesiumViewer = () => {
 };
 
 export default CesiumViewer
-// class CesiumViewer extends Component {
-//   public viewer: Cesium.Viewer | undefined
-  
-//   componentDidMount() {
-//     if (this.viewer) {
-//       const position = Cesium.Cartesian3.fromDegrees(106.774124, -6.200000, 100)
-//       //Actually create the entity
-//       const entity = this.viewer.entities.add({
-//         position: position,
-//         //Load the Cesium plane model to represent the entity
-//         model: {
-//           uri:"/models/Cesium_Air.glb",
-//           minimumPixelSize: 64,
-//         },
-//       })
-
-//       const drag = new CesiumEntity(this.viewer)
-//       drag.enable()
-      
-//       this.viewer.zoomTo(
-//         this.viewer.entities,
-//         new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-90))
-//       )
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <Viewer full
-//         ref={e => {
-//           this.viewer = e ? e.cesiumElement : undefined
-//         }}
-//         infoBox={true}
-//         selectionIndicator={false}
-//         shouldAnimate={true}
-//       />
-//     );
-//   }
-// }
-
-// export default CesiumViewer
