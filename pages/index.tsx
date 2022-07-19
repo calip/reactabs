c9cb4e7f82667114a67390c7f61757c63dd11351
alip putra <asalip.putra@gmail.com>
2022-07-19 14:48:42 +0700

    load api use function useeffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
 import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
+import Entity from '../helpers/entity'
 
 const CesiumViewer = dynamic(
   () => import('../components/cesiumViewer'),
   { ssr: false }
 )
 
-function Index() {
+export default function Index(props: any) {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true)
+      const data = await Entity.get()
+      setData(data)
+      setLoading(false)
+    })()
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+
   return (
-    <CesiumViewer />
+    <CesiumViewer entities={data} />
   )
 }
-
-export default Index
