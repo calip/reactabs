265e9d79afee21677fb73b61f823133cd7755582
alip putra <asalip.putra@gmail.com>
2022-07-19 11:47:16 +0700

    refactor cesium viewer use function component

--- components/cesiumViewer.tsx
+++ components/cesiumViewer.tsx
@@ -1,17 +1,17 @@
-import React, { Component } from "react"
-import { Viewer } from "resium"
+import { useRef, useEffect } from "react"
 import * as Resium from "resium"
 import * as Cesium from 'cesium'
 import CesiumEntity from "./cesiumEntity"
 
-class CesiumViewer extends Component {
-  public viewer: Cesium.Viewer | undefined
-  
-  componentDidMount() {
-    if (this.viewer) {
+
+const CesiumViewer = () => {
+  const ref = useRef<Resium.CesiumComponentRef<Cesium.Viewer>>(null)
+
+  useEffect(() => {
+    if(ref.current?.cesiumElement) {
       const position = Cesium.Cartesian3.fromDegrees(106.774124, -6.200000, 100)
       //Actually create the entity
-      const entity = this.viewer.entities.add({
+      const entity = ref.current?.cesiumElement.entities.add({
         position: position,
         //Load the Cesium plane model to represent the entity
         model: {
@@ -20,28 +20,58 @@ class CesiumViewer extends Component {
         },
       })
 
-      const drag = new CesiumEntity(this.viewer)
+      const drag = new CesiumEntity(ref.current?.cesiumElement)
       drag.enable()
       
-      this.viewer.zoomTo(
-        this.viewer.entities,
+      ref.current?.cesiumElement.zoomTo(
+        ref.current?.cesiumElement.entities,
         new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-90))
       )
     }
-  }
+  }, [])
 
-  render() {
-    return (
-      <Viewer full
-        ref={e => {
-          this.viewer = e ? e.cesiumElement : undefined
-        }}
-        infoBox={true}
-        selectionIndicator={false}
-        shouldAnimate={true}
-      />
-    );
-  }
-}
+  return <Resium.Viewer ref={ref} />
+};
 
 export default CesiumViewer
+// class CesiumViewer extends Component {
+//   public viewer: Cesium.Viewer | undefined
+  
+//   componentDidMount() {
+//     if (this.viewer) {
+//       const position = Cesium.Cartesian3.fromDegrees(106.774124, -6.200000, 100)
+//       //Actually create the entity
+//       const entity = this.viewer.entities.add({
+//         position: position,
+//         //Load the Cesium plane model to represent the entity
+//         model: {
+//           uri:"/models/Cesium_Air.glb",
+//           minimumPixelSize: 64,
+//         },
+//       })
+
+//       const drag = new CesiumEntity(this.viewer)
+//       drag.enable()
+      
+//       this.viewer.zoomTo(
+//         this.viewer.entities,
+//         new Cesium.HeadingPitchRange(0, Cesium.Math.toRadians(-90))
+//       )
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <Viewer full
+//         ref={e => {
+//           this.viewer = e ? e.cesiumElement : undefined
+//         }}
+//         infoBox={true}
+//         selectionIndicator={false}
+//         shouldAnimate={true}
+//       />
+//     );
+//   }
+// }
+
+// export default CesiumViewer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic'
-import Entity from '../helpers/entity';
 
 const CesiumViewer = dynamic(
   () => import('../components/cesiumViewer'),
