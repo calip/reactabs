891f767351209b47abfee424d28e28305e00b0d3
alip putra <asalip.putra@gmail.com>
2022-07-25 18:52:35 +0700

    add entity default asset

---
+++ components/entityDefault.tsx
@@ -0,0 +1,25 @@
+import * as Cesium from 'cesium'
+
+const EntityDefault = () => {
+  const result = {
+    Callsign: "F-16 A/B",
+    DISTypeID: {
+        Kind: 1,
+        Domain: 2,
+        Category: 2,
+        Subcategory: 0,
+        Country: 0,
+        Extra: 0,
+        Specifics: 0
+    },
+    VisualModel: {
+        "def": "/models/Cesium_Air.glb"
+    },
+    state: {
+        rotOffset: 0
+    }
+  }
+  return result
+}
+
+export default EntityDefault
