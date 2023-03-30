85dac8eaa7ba65fc18d100b93251cd7b877eaaa7
alip putra <asalip.putra@gmail.com>
2023-03-30 14:25:53 +0700

    fix helper clone object function target

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -22,14 +22,13 @@ const helpers = {
       return result
     })
   },
-  cloneObject: (ref: any, target: any = null) => {
-    if (!target) const tgt: any = {}
-    Object.keys(ref).map((key) => {
-      if (!Object.prototype.hasOwnProperty.call(ref, key)) {
-        tgt[key] = ref[key]
+  cloneObject: (ref: any, tgt: any = null) => {
+    if (!tgt) var tgt: any = {}
+    for (const el in ref) {
+      if (ref.hasOwnProperty(el)) {
+        tgt[el] = ref[el]
       }
-      return tgt
-    })
+    }
     return tgt
   },
   cartoDelta: (ca: any, cb: any, ch = 0.0) => {
